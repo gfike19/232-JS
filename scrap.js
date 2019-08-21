5bd3e3a197cdbc65806bd0b13044bdbd01ad6ae8
gfike19@gmail.com <Vaughn*573>
2019-08-21 15:51:03 -0500

    go to exercise 3

---
+++ scrap.js
@@ -0,0 +1,30 @@
+            // for(var key in fruit) {
+            //     var value = fruit[key];
+            //     document.getElementById("allFruit").innerHTML = fruit.keys;
+            // }
+
+            // doesn't work cause fruit is a map
+            // document.getElementById("allFruit").innerHTML = fruit.prototype.array.forEach(element => {
+            //     document.writeln(element);
+            // });
+//             fruit.forEach(function (element) {
+
+// });
+
+            // below does nothing at all
+            // console.log(fruit);
+            // window.alert(fuit);
+
+                    // nada
+        // function display(values){
+        //     document.writelin(values + "<br>");
+        // }
+
+        // nada as well,tried changing display to use alerts, nothin
+        // alert(fruit);
+
+        // below doesnt work
+        // fruit.forEach(display);
+        
+            // nada
+        // <!-- <span onload="Exer1()"></span> -->
